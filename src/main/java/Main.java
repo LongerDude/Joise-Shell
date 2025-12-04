fd455ea446f71460d761412c8d8b01f7dfe1ab8e
salmane <salmajo2001@gmail.com>
2025-12-04 15:30:08 +0100

    Type path crawl
    used sys env to grab path string, split it using file.pathseperator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> commands = new ArrayList<>();
         commands.add("echo");
@@ -15,24 +20,21 @@ public class Main {
             System.out.print("$ ");
             String command = scanner.nextLine();
             List<String> parts = List.of(command.split(" "));
-            if (getCommand(parts).equals("exit")){
+            if (getCommand(parts).equals("exit")) {
                 break;
             }
-            if (getCommand(parts).equals("echo")){
+            if (getCommand(parts).equals("echo")) {
                 System.out.println(command.substring(command.indexOf(" ") + 1));
                 continue;
             }
-            if (getCommand(parts).equals("type")){
-                if (commands.contains(parts.get(1))){
+            if (getCommand(parts).equals("type")) {
+                if (commands.contains(parts.get(1))) {
                     System.out.println(parts.get(1) + " is a shell builtin");
                     continue;
                 } else {
-                    StringBuilder builder = new StringBuilder(parts.get(1));
-                    builder.append(": not found ");
-                    System.out.println(builder.toString());
+                    findInPath(parts.get(1));
                     continue;
                 }
-
             }
 
 
@@ -42,9 +44,37 @@ public class Main {
         }
 
 
-
     }
-    public static String getCommand(List<String> command){
+
+    public static String getCommand(List<String> command) {
         return command.get(0);
     }
+
+    public static void findInPath(String command) {
+        String PATH = System.getenv("PATH");
+        if (PATH != null) {
+            String[] directories = PATH.split(File.pathSeparator);
+            Optional<Path> opt = null;
+            for (String directoryPath : directories) {
+                Path path = Paths.get(directoryPath);
+                if (Files.isDirectory(path)) {
+                    try (Stream<Path> paths = Files.walk(path, 1)) {
+                        opt = paths.filter(Files::isExecutable).filter(file -> file.toFile().getName().equals(command)).findAny();
+                        if (opt.isPresent()) {
+                            System.out.println(command +" is "+opt.get().getParent() + "/" + opt.get().getFileName());
+                            return;
+                        }
+
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                }
+            }
+            System.out.println(command+": not found");
+
+        }
+    }
+
 }
+
